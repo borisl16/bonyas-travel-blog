24baae4d7275dc6999b44a57100fbbd2b159b753
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-04-06 14:55:23 +0100

    authenticate and controller authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-
+  
+  root to: 'home#index'
   devise_for :authors
-  root 'home#index'
+
+  scope module: 'authors' do
+    resources :posts
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

0551d2a1d88f9ffc1333bc0604e648872834faca
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-04-07 14:00:30 +0100

    element controller, adding images,videos to the blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   
+  resources :elements
   root to: 'home#index'
   devise_for :authors
 
   scope module: 'authors' do
-    resources :posts
+    resources :posts do
+      resources :elements
+    end
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

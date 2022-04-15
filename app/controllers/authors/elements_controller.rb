2c0cd143d50412f4eacb57718530e2e3b31881ec
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-04-15 10:01:56 +0100

    adding image function

--- app/controllers/authors/elements_controller.rb
+++ app/controllers/authors/elements_controller.rb
@@ -43,7 +43,7 @@ module Authors
     # DELETE /elements/1
     def destroy
       @element.destroy
-      redirect_to elements_url, notice: 'Element was successfully destroyed.'
+      redirect_to edit_post_path(@element.post)
     end
 
     private
@@ -59,7 +59,7 @@ module Authors
 
       # Only allow a list of trusted parameters through.
       def element_params
-        params.require(:element).permit(:element_type, :content)
+        params.require(:element).permit(:element_type, :content, :image)
       end
   end
 end
